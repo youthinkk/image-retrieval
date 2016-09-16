30608a22a88e054a684b5659fc7482be8349f769
ShumShum93 <a0121823@u.nus.edu>
2016-09-16 21:48:44 +0800

    Keywords.py

--- src/keywords.py
+++ src/keywords.py
@@ -1,3 +1,47 @@
 """
 Template for Visual Keyword
 """
+import os
+import cv2
+
+# creeate SIFT object
+detector = cv2.SIFT()
+
+#identify OS to run SIFT
+def process_image(imagename, resultname):
+    """ process an image and save the results in a .key ascii file"""
+    # check if linux or windows
+    if os.name == "nt":
+        cmd = "siftWin32 <" + imagename + ">" + resultname
+    else:
+        cmd = "./sift <" + imagename + ">" + resultname
+    os.system(cmd)
+    print 'processed', imagename
+
+#***************************************************************************
+
+#generate keypoints for the image
+def findKp(img):
+    return detector.detect(img)
+
+def runSIFT(image1, image2):
+    #gray out both imgs
+    gray1 = cv2.cvtColor(image1,cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
+
+    #finding the keypoints for each individual image
+    kp1 = findKp(gray1)
+    kp2 = findKp(gray2)
+
+    print kp1
+    #draw kp on img
+    newImg1 = cv2.drawKeypoints(gray1, kp1)
+    newImg2 = cv2.drawKeypoints(gray2, kp2)
+    # cv2.imwrite('testOriginal.jpg', gray1)
+    # cv2.imwrite('test.jpg', newImg1)
+
+    #comparing the keypoints of the 2 images
+
+
+
+    return 0
