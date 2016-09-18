d73d189b5f847d4918296d1df2cded00e7b94de4
Yu Ting <youthinkk@outlook.com>
2016-09-18 19:38:51 +0800

    Add class lookup table

--- src/learning.py
+++ src/learning.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import sys
 import tarfile
 import os.path
@@ -5,6 +21,7 @@ import re
 import numpy as np
 import tensorflow as tf
 import urllib
+import learning_index as index
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('MODEL_DIR', '/tmp/imagenet',
@@ -29,6 +46,7 @@ class NodeLookup(object):
         if not uid_lookup_path:
             uid_lookup_path = os.path.join(FLAGS.MODEL_DIR,
                                            'imagenet_synset_to_human_label_map.txt')
+        print uid_lookup_path
         self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
 
     def load(self, label_lookup_path, uid_lookup_path):
@@ -112,7 +130,7 @@ def inference(image_path):
         for node_id in top_k:
             human_string = node_lookup.id_to_string(node_id)
             score = predictions[node_id]
-            name_score.append((human_string.split(',')[0], score))
+            name_score.append((human_string, score, node_id))
 
         return name_score
 
