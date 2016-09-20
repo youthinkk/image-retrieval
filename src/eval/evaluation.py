41c6292d50f02e26dd34535daa33777e58f9a9ad
Hong Chao <jianghc93@hotmail.com>
2016-09-20 13:06:44 +0800

    Added SVM trainer

--- src/eval/evaluation.py
+++ src/eval/evaluation.py
@@ -1,6 +1,6 @@
 import numpy as np
-from numpy import linalg as LA
-from src.file import get_color_index
+import cv2
+from src.file import get_index
 from src.core import color_similarity
 
 K_SIZE = 16
@@ -10,10 +10,6 @@ TRAIN_COLOR_INDEX_PATH = INDEX_FOLDER + "/train_histogram.csv"
 TEST_COLOR_INDEX_PATH = INDEX_FOLDER + "/test_histogram.csv"
 TRAIN_SIFT_INDEX_PATH = INDEX_FOLDER + "/train_sift.csv"
 TEST_SIFT_INDEX_PATH = INDEX_FOLDER + "/test_sift.csv"
-
-
-def cosine_similarity(a, b):
-    return np.inner(a, b) / (LA.norm(a) * LA.norm(b))
 
 
 def local_accuracy(top_k, test_label):
@@ -27,8 +23,8 @@ def local_accuracy(top_k, test_label):
 
 
 def eval_color():
-    test_dict = get_color_index(TEST_COLOR_INDEX_PATH)
-    train_dict = get_color_index(TRAIN_COLOR_INDEX_PATH)
+    test_dict = get_index(TEST_COLOR_INDEX_PATH)
+    train_dict = get_index(TRAIN_COLOR_INDEX_PATH)
 
     test_size = len(test_dict)
     accuracy_sum = 0.
@@ -53,8 +49,8 @@ def eval_color():
 
 
 def eval_sift():
-    test_dict = get_color_index(TEST_SIFT_INDEX_PATH)
-    train_dict = get_color_index(TRAIN_SIFT_INDEX_PATH)
+    test_dict = get_index(TEST_SIFT_INDEX_PATH)
+    train_dict = get_index(TRAIN_SIFT_INDEX_PATH)
 
     test_size = len(test_dict)
     accuracy_sum = 0.
@@ -67,7 +63,7 @@ def eval_sift():
         for train_name, train_value in train_dict.iteritems():
             train_label = train_value[0]
             train_color = train_value[1]
-            similarity = cosine_similarity(test_color, train_color)
+            similarity = color_similarity(test_color, train_color)
             results.append((train_label, similarity))
 
         top_k = sorted(results, key=lambda x: x[1], reverse=True)[:K_SIZE]
@@ -78,9 +74,46 @@ def eval_sift():
     return accuracy
 
 
+def train_svm():
+    f = open('../../data/category_names.txt', 'r')
+    categories = [item.strip('\r\n') for item in list(f)]
+    f.close()
+
+    train_desc = []
+    train_labels = []
+
+    train_dict = get_index(TRAIN_SIFT_INDEX_PATH)
+
+    for train_name, train_value in train_dict.iteritems():
+        train_label = train_value[0]
+        train_sift = train_value[1].astype("float32")
+        train_desc.append(np.array(train_sift))
+        train_labels.append(categories.index(train_label))
+
+    print len(train_desc)
+    print len(train_labels)
+    svm = cv2.SVM()
+    svm.train(np.array(train_desc), np.array(train_labels))
+    return svm
+
 # Color histogram accuracy
 # Using Bhattacharyya distance: 0.0329166666667
 # Using chi2 distance: 0.0358333333333
 # print "Color histogram accuracy: ", eval_color()
 
 print "SIFT accuracy: ", eval_sift()
+
+# svm = train_svm()
+# test_dict = get_color_index(TEST_SIFT_INDEX_PATH)
+# score = 0
+# f = open('../../data/category_names.txt', 'r')
+# categories = [item.strip('\r\n') for item in list(f)]
+# f.close()
+# for test_name, test_value in test_dict.iteritems():
+#     test_label = test_value[0]
+#     test_color = test_value[1]
+#     prediction = svm.predict(test_color.astype("float32"))
+#     if categories.index(test_label) == prediction:
+#         score += 1
+#
+# print score
