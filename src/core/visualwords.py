78c5f1055a7f2e53b479c8939908deed7c2a4549
Hong Chao <jianghc93@hotmail.com>
2016-09-20 13:05:14 +0800

    Refactored visualwords

--- src/core/visualwords.py
+++ src/core/visualwords.py
@@ -2,54 +2,37 @@ import cv2
 import glob
 import pickle
 
-##############################################################
-# Uncomment the following to generate the visual vocab
-##############################################################
-# dictionarySize = 10      # NEED TO CHANGE THIS...
-# sift = cv2.SIFT()
-# BOW = cv2.BOWKMeansTrainer(dictionarySize)
-#
-# for p in glob.glob("../data/train/*.jpg"):
-#     image = cv2.imread(p)
-#     gray = cv2.cvtColor(image, cv2.CV_LOAD_IMAGE_GRAYSCALE)
-#     kp, dsc= sift.detectAndCompute(gray, None)
-#     BOW.add(dsc)
-#
-# #dictionary creation
-# print "Start of clustering"
-# dictionary = BOW.cluster()
-# print "End of clustering"
-# fileObject = open("../data/index/visual_vocab",'wb')
-# pickle.dump(dictionary, fileObject)
-# fileObject.close()
 
-sift = cv2.SIFT()
-fileObject = open("../data/index/visual_vocab", 'r')
-dictionary = pickle.load(fileObject)
+class SIFTDescriptor:
 
-sift2 = cv2.DescriptorExtractor_create("SIFT")
-bowDiction = cv2.BOWImgDescriptorExtractor(sift2, cv2.BFMatcher(cv2.NORM_L2))
-bowDiction.setVocabulary(dictionary)
+    def __init__(self, vocab_path):
+        file_object = open(vocab_path, 'r')
+        vocab = pickle.load(file_object)
+
+        sift2 = cv2.DescriptorExtractor_create("SIFT")
+        bow_dictionary = cv2.BOWImgDescriptorExtractor(sift2, cv2.BFMatcher(cv2.NORM_L2))
+        bow_dictionary.setVocabulary(vocab)
+
+        self.bow_dictionary = bow_dictionary
+        self.sift = cv2.SIFT()
+
+    def describe(self, img):
+        return self.bow_dictionary.compute(img, self.sift.detect(img))[0]
 
 
-def generate_sift_index_file(folder_directory, output_path):
-    file_paths = glob.glob(folder_directory + "/*.jpg")
-    output = open(output_path, "w")
+def train_bow(dictionary_size):
+    sift = cv2.SIFT()
+    BOW = cv2.BOWKMeansTrainer(dictionary_size)
 
-    for path in file_paths:
-        image_id = path[path.rfind("/") + 1:]
-        label = image_id.split("_")[-1][:-4]
+    for p in glob.glob("../data/train/*.jpg"):
+        image = cv2.imread(p, 0)
+        kp, dsc = sift.detectAndCompute(image, None)
+        BOW.add(dsc)
 
-        image = cv2.imread(path)
-        sift_feature = bowDiction.compute(image, sift.detect(image))
-        sift_feature = [str(f) for f in sift_feature[0]]
-        output.write("%s,%s,%s\n" % (image_id, label, ",".join(sift_feature)))
-
-    output.close()
-
-
-# Generate histogram index file for training images
-generate_sift_index_file("../data/train", "train_sift.csv")
-
-# Generate histogram index file for testing images
-generate_sift_index_file("../data/test", "test_sift.csv")
+    # dictionary creation
+    print "Start of clustering"
+    dictionary = BOW.cluster()
+    print "End of clustering"
+    fileObject = open("../data/index/visual_vocab", 'wb')
+    pickle.dump(dictionary, fileObject)
+    fileObject.close()
