8e1a726d43c7e958aeaaaaff898fe5eacdf44ef8
ShumShum93 <a0121823@u.nus.edu>
2016-09-23 20:30:12 +0800

    Inclusion of synonyms in text search

--- src/core/text.py
+++ src/core/text.py
@@ -1,8 +1,8 @@
 """
 Template for Text Features
 """
-from PIL import Image, ExifTags
-from PIL.ExifTags import TAGS
+from stemming.porter2 import stem
+from nltk.corpus import stopwords, wordnet
 
 #Specify file path of training and test text tags
 train_Tags = "../data/train_text_tags.txt"
@@ -25,8 +25,17 @@ def processFile(path):
             count += 1
     return l
 
+#returns a list of synonyms of the word
+def synonym(word):
+    synonyms = list()
+    for syn in wordnet.synsets(word):
+        for l in syn.lemmas():
+            synonyms.append(l.name())
+    synonyms = list((set(synonyms)))
+    return synonyms
+
 #search for images associated to texts(training data)
-def queryTrainText(query):
+def singleQueryTrainText(query):
     train_list = processFile("../data/train_text_tags.txt")
     train_list_len = len(train_list)
 
@@ -39,6 +48,7 @@ def queryTrainText(query):
         j = 1
         single_list_len = len(train_list[i]) #loop through single line
 
+        #adding matched images into dictionary under the key of the matched keyword
         while (j < single_list_len):
             if (query.lower() in train_list[i][j].lower()) and (not matched.has_key(train_list[i][j])): #new key
                 matched[train_list[i][j]] = [train_list[i][0].replace('.jpg','')] #make a list in the key
@@ -53,7 +63,7 @@ def queryTrainText(query):
     return matched
 
 #search for images associated to texts(test data)
-def queryTestText(query):
+def singleQueryTestText(query):
     test_list = processFile("../data/test_text_tags.txt")
     test_list_len = len(test_list)
 
@@ -79,18 +89,27 @@ def queryTestText(query):
 
     return matched
 
-# #search of tags of image and
-# def newImageQuery(image_path):
-#     img = Image.open(image_path)
-#
-#     tag_name_to_id = dict([(v, k) for k, v in ExifTags.TAGS.items()])
-#
-#     exif_data = img._getexif()
-#     exif = {
-#         TAGS[k]: v
-#         for k, v in img._getexif().items()
-#         if k in TAGS
-#         }
-#     print exif_data
-#     print exif
-#     return 0
+#free text search
+def freeTextSearch(string_query):
+    syns = wordnet.synsets("program")
+
+    s = set(stopwords.words('english')) #set for stemming
+    input_query_list = filter(lambda w: not w in s, string_query.split()) #list of non stop words
+    for i in range(len(input_query_list)): #stem and resolve all query words
+        input_query_list[i] = stem(input_query_list[i])
+    input_query_list = list(set(input_query_list)) #remove duplicates
+
+    full_query_list = input_query_list #define a full query list which will include all synonym
+    for i in range(len(input_query_list)):
+        full_query_list = full_query_list + synonym(input_query_list[i])
+    full_query_list = list(set(full_query_list)) #this is the COMPLETE set of queries
+
+    #for i in range(len(query_list)): #concat all dictionay to make one big dict
+
+
+    testing1 = singleQueryTrainText(input_query_list[0])
+    testing2 = singleQueryTrainText(input_query_list[3])
+    testing1.update(testing2)
+    #print testing1
+
+    return string_query
